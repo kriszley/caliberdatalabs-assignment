64a17e557b11f11c85a07ca9d7d3a2a483c228aa
Kris Lee <kriszley>
2020-11-19 01:18:46 -0800

    Complete documentation

--- glare-detector/app/mod_glare/controllers.py
+++ glare-detector/app/mod_glare/controllers.py
@@ -10,20 +10,38 @@ glare = Glare()
 # Set the route and accepted methods
 @mod_glare.route('/detect', methods=['POST'])
 def detect():
+    """
+    API that process the metadata requests and determines if there is a
+    possibility of direct glare in the associated image or not.
+    ---
+    requests:
+        {
+            "lat": a float between 0 to 90 that shows the latitude in which the image was taken
+            "lon": a float between -180 to 180 that shows the longitude in which the image was taken
+            "epoch": Linux epoch in second
+            "orientation": a float between -180 and 180 the east-ward orientation of car travel from
+                           true north. 0 means nort. 90 is east and -90 is west.
+        }
+
+    responses:
+      500:
+        { 
+            'status': Shows the "error" status.
+            'detail': Description of the error.
+        }
+      200:
+        {
+            'status': Shows the "success" status.
+            "glare": True if glare is present, False otherwise.
+        }
+    """
+    # Store request body as a metadata variable
     metadata = request.get_json()
 
-    result = glare.detect_glare(metadata)
-    if result['status'] == "error":
-        return jsonify(result)
-
-    res = {
-        "glare": result['glare']
-    }
-
-    return jsonify(res)
-    
-    # Limitation??
-    
-    # Conditions??
-
+    # Call detect_glare service function
+    res = glare.detect_glare(metadata)
+    if res['status'] == "error":
+        # Return error if not successful
+        return jsonify(res), 500
+    return jsonify(res), 200
     
--- glare-detector/app/mod_glare/services.py
+++ glare-detector/app/mod_glare/services.py
@@ -3,14 +3,31 @@ import datetime
 
 class Glare:
     def detect_glare(self, metadata):
+        """ Given the metadata, determines whether there is a possible glare.
+        Args:
+            metadata (json): The metadata JSON object:
+            Example.
+                    {
+                        "lat": 49.2699648,
+                        "lon": -123.1290368,
+                        "epoch": 1588704959.321,
+                        "orientation": -10.2
+                    }
+
+        Returns:
+            dict: The return dict object that contains 
+                `status` and `glare` values if successful, and `status` and `details` otherwise.
+        """
         try:
+            # Validate input metadata
             is_valid = self.validate_metadata(metadata)
             if is_valid != True:
-                print(str(is_valid))
                 raise Exception(str(is_valid))
 
+            # Glare set to False by default
             is_glare = "false"
 
+            # Assign metadata into variables
             lat = metadata['lat']
             lon = metadata['lon']
             epoch = metadata['epoch']
@@ -41,28 +58,48 @@ class Glare:
         except Exception as e:
             return { 'status': "error", 'detail': str(e) }
     
-    # If azimuth > 180 degrees, convert to negative
     def to_neg_degree(self, degree):
+        """ Converts the given positive angle degree (>180) into negative value.
+        Args:
+            degree (float): Positive angle degree value
+
+        Returns:
+            degree (float): Converted negative angle degree value
+        """
         if degree > 180:
             return degree - 360
         return degree
 
     # Validate metadata
     def validate_metadata(self, metadata):
+        """ Validate whether all of metadata meet the conditions.
+        Args:
+            metadata (json): The metadata JSON object:
+            Example.
+                    {
+                        "lat": 49.2699648,
+                        "lon": -123.1290368,
+                        "epoch": 1588704959.321,
+                        "orientation": -10.2
+                    }
+
+        Returns:
+            bool: `True` if pass all the conditions.
+            e: Exception object if fail one of the conditions.
+        """
         try:
             lat = metadata['lat']
-            if not isinstance(lat, float) and not 0 < lat < 90:
+            if not isinstance(lat, float) or not 0 < lat < 90:
                 raise Exception("Latitude: " + str(lat) + " is not a float within 0 to 90.")
             lon = metadata['lon']
-            if not isinstance(lon, float) and not -180 < lon < 180:
+            if not isinstance(lon, float) or not -180 < lon < 180:
                 raise Exception("Longitude: " + str(lon) + " is not a float within -180 to 180.")
             epoch = metadata['epoch']
             if not 0 < epoch:
                 raise Exception("Epoch: " + str(epoch) + " is not a float within 0 to 90.")
             orientation = metadata['orientation']
-            if not isinstance(orientation, float) and not -180 < orientation < 180:
+            if not isinstance(orientation, float) or not -180 < orientation < 180:
                 raise Exception("Orientation: " + str(orientation) + " is not a float within -180 to 180.")
             return True
         except Exception as e:
-            print("HERE")
             return e
